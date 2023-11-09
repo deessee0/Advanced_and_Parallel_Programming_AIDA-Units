e20e07ff13341a8d24eaf4ef3bbf676ece88ade2
Alberto <alberto.disalvia@gmail.com>
2023-11-09 14:47:42 +0100

    erroe in file_write

--- Lecture14/file_write.c
+++ Lecture14/file_write.c
@@ -4,7 +4,7 @@ int main(int argc, char * argv[])
 {
   FILE * fp = fopen("test.txt", "w");
   for (int i = 0; i < 100; i++) {
-    fprintf(fp, "i = %d\n");
+    fprintf(fp, "i = %d\n", i);
   }
   fclose(fp);
   return 0;

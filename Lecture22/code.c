5011c873c556593ea94bd4a3c296bee060da78e9
Alberto <alberto.disalvia@gmail.com>
2023-11-30 15:10:16 +0100

    Lez22_FIX

---
+++ Lecture22/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int sum(int a, int b)
+{
+  int res = a + b;
+  printf("Somma di %d e %d con risultato %d\n", a, b, res);
+  return res;
+}
+
+float multiply(int a, float b)
+{
+  float res = a * b;
+  printf("Moltiplicaizone di %d e %f con risultato %f\n", a, b, res);
+  return res;
+}
+
+void swap(int * a, int * b)
+{
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}

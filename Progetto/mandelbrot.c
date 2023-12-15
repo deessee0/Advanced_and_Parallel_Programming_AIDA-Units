d33ad68ef90aa31c278cc67f49d3b3ce69032562
Alberto <alberto.disalvia@gmail.com>
2023-12-15 15:35:37 +0100

    Inizio progetto

---
+++ Progetto/mandelbrot.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <complex.h>
+
+void isMandelbrot(double complex n, float r = 2, int M)
+{
+    const double complex c = 0.0 + 0.0 * I;
+    
+
+    for(int i=0; i<M,i++){
+        Zn = 
+        
+        if (n > 0)
+        {
+            double complex zn = (zn * zn) + z0;
+        }
+    }
+}
